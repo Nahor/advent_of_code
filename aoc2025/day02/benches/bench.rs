f61c28954991974ec5295d8be1771f9adb56056c
Nahor <nahor.j+github@gmail.com>
2025-12-04 08:53:53 -0800

    2025 day 02: Chris Biscardi version
    Added Chris ilog10 version of part1 for reference and my modified
    version that skips over known good IDs

--- aoc2025/day02/benches/bench.rs
+++ aoc2025/day02/benches/bench.rs
@@ -15,6 +15,20 @@ mod part1_bench {
             .with_inputs(|| read_input_str!(None).unwrap())
             .bench_values(|content| part1::run(&content).unwrap());
     }
+
+    #[divan::bench(name = "1_chrisbiscardi_ref")]
+    fn chrisbiscardi_ref(bencher: divan::Bencher) {
+        bencher
+            .with_inputs(|| read_input_str!(None).unwrap())
+            .bench_values(|content| part1_chrisbiscardi_ref::run(&content).unwrap());
+    }
+
+    #[divan::bench(name = "2_chrisbiscardi_skip")]
+    fn chrisbiscardi_skip(bencher: divan::Bencher) {
+        bencher
+            .with_inputs(|| read_input_str!(None).unwrap())
+            .bench_values(|content| part1_chrisbiscardi_skip::run(&content).unwrap());
+    }
 }
 
 mod part2_bench {
